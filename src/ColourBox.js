c95e62032bb5b28416fb107e5b34da1934291fb9
Oybek Abdulazizov <oybek.tulqinovich@gmail.com>
2023-04-14 12:19:49 +0200

    style ColourBox

--- src/ColourBox.js
+++ src/ColourBox.js
@@ -2,10 +2,14 @@ import React from 'react';
 
 import './ColourBox.css';
 
-export default function ColourBox({ bgColour }) {
+export default function ColourBox({ background, name }) {
   return (
-    <div className='ColourBox' style={{ backgroundColor: bgColour }}>
-      <span>MORE</span>
+    <div className='ColourBox' style={{ background }}>
+      <button className='copy-btn'>Copy</button>
+      <div className='box-content'>
+        <span>{name}</span>
+        <button className='btn-more'>More</button>
+      </div>
     </div>
   );
 }
--- src/Palette.js
+++ src/Palette.js
@@ -6,7 +6,7 @@ import ColourBox from './ColourBox';
 
 export default function Palette({ palette }) {
   const colourBoxes = palette.colours.map((c, id) => (
-    <ColourBox bgColour={c.colour} key={id} />
+    <ColourBox background={c.colour} name={c.name} key={id} />
   ));
   return (
     <div className='Palette'>
