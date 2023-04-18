82ae836494ec53ccbcea178df07dc407df25aad6
Oybek Abdulazizov <oybek.tulqinovich@gmail.com>
2023-04-18 16:55:27 +0200

    add a drop-down menu for colour formats

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Slider from 'rc-slider';
 import 'rc-slider/assets/index.css';
+import { MenuItem, Select } from '@mui/material';
 
 import './NavBar.css';
 
-export default function NavBar({ level, changeLevel }) {
+export default function NavBar({ level, changeLevel, changeFormat }) {
+  const [state, setState] = useState({ format: 'hex' });
+
+  function handleChangeLevel(level) {
+    changeLevel(level);
+  }
+
+  function handleChangeFormat(e) {
+    setState((prevState) => ({ ...prevState, format: e.target.value }));
+    changeFormat(e.target.value);
+  }
+
   return (
     <nav className='NavBar'>
       <div className='logo'>
@@ -18,9 +30,16 @@ export default function NavBar({ level, changeLevel }) {
             min={100}
             max={900}
             step={100}
-            onChange={changeLevel}
+            onChange={handleChangeLevel}
           />
         </div>
+      </div>
+      <div className='select-container'>
+        <Select value={state.format} onChange={handleChangeFormat}>
+          <MenuItem value='hex'>HEX </MenuItem>
+          <MenuItem value='rgb'>RGB </MenuItem>
+          <MenuItem value='rgba'>RGBA</MenuItem>
+        </Select>
       </div>
     </nav>
   );
--- src/Palette.js
+++ src/Palette.js
@@ -6,21 +6,27 @@ import ColourBox from './ColourBox';
 import NavBar from './NavBar';
 
 export default function Palette({ palette }) {
-  const [state, setState] = useState({ level: 500 });
+  const [state, setState] = useState({ level: 500, format: 'hex' });
+
   function changeLevel(level) {
-    setState((prevState) => ({
-      ...prevState,
-      level,
-    }));
+    setState((prevState) => ({ ...prevState, level }));
+  }
+
+  function changeFormat(val) {
+    setState((prevState) => ({ ...prevState, format: val }));
   }
 
   const colourBoxes = palette.colours[state.level].map((colour, id) => (
-    <ColourBox background={colour.hex} name={colour.name} key={id} />
+    <ColourBox background={colour[state.format]} name={colour.name} key={id} />
   ));
 
   return (
     <div className='Palette'>
-      <NavBar level={state.level} changeLevel={changeLevel} />
+      <NavBar
+        level={state.level}
+        changeLevel={changeLevel}
+        changeFormat={changeFormat}
+      />
       <div className='Palette-colours'>{colourBoxes}</div>
     </div>
   );
