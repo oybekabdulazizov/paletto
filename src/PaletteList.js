7ee849b30482e0f70392c431ba28f0f7a3363b2b
Oybek Abdulazizov <oybek.tulqinovich@gmail.com>
2023-04-22 10:42:47 +0200

    install mui-styled-components and style the Palette component

--- src/PaletteList.js
+++ src/PaletteList.js
@@ -1,14 +1,51 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+import MiniPalette from './MiniPalette';
+
+const PaletteListContaier = styled('div')(() => ({
+  backgroundColor: 'blue',
+  height: '100vh',
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'flex-start',
+  '& .container': {
+    width: '50%',
+    display: 'flex',
+    alignItems: 'flex-start',
+    flexDirection: 'column',
+    flexWrap: 'wrap',
+  },
+  '& .nav': {
+    width: '100%',
+    display: 'flex',
+    justifyContent: 'space-between',
+    color: 'white',
+  },
+  '& .palettes': {
+    boxSizing: 'border-box',
+    width: '100%',
+    display: 'grid',
+    gridTemplateColumns: 'repeat(3, 30%)',
+    gridGap: '5%',
+  },
+}));
 
 export default function PaletteList({ seedPalette }) {
   return (
-    <div>
-      {seedPalette.map((palette) => (
-        <p key={palette.id}>
-          <Link to={`/palette/${palette.id}`}>{palette.paletteName}</Link>
-        </p>
-      ))}
-    </div>
+    <PaletteListContaier>
+      <div className='container'>
+        <nav className='nav'>
+          <h1 className='title'>Paletto Colours</h1>
+        </nav>
+        <div className='palettes'>
+          {seedPalette.map((palette) => (
+            <div key={palette.id}>
+              <MiniPalette {...palette} />
+            </div>
+          ))}
+        </div>
+      </div>
+    </PaletteListContaier>
   );
 }
