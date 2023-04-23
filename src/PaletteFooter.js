52f9d95eddee5c9b46b9e4c8371f370ea7489459
Oybek Abdulazizov <oybek.tulqinovich@gmail.com>
2023-04-23 16:46:15 +0200

    move palette footer styles from Palette.css into the PaletteFooter component as jss

--- src/PaletteFooter.js
+++ src/PaletteFooter.js
@@ -1,11 +1,25 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const PaletteFooterContainer = styled('div')(() => ({
+  height: '4vh',
+  display: 'flex',
+  flexDirection: 'row',
+  alignItems: 'center',
+  justifyContent: 'flex-end',
+  paddingRight: '1em',
+  fontWeight: '500',
+  fontSize: '1.1em',
+  '.name': {
+    marginRight: '0.5em',
+  },
+}));
 
 export default function PaletteFooter({ name, emoji }) {
   return (
-    <footer className='Palette-footer'>
-      <span>
-        {name} <span>{emoji}</span>
-      </span>
-    </footer>
+    <PaletteFooterContainer>
+      <span className='name'>{name}</span>
+      <span className='emoji'>{emoji}</span>
+    </PaletteFooterContainer>
   );
 }
