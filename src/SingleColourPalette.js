7eeb561dc6060f44542ad81f2c5cee3cd7279773
Oybek Abdulazizov <oybek.tulqinovich@gmail.com>
2023-04-23 16:47:07 +0200

    style the SingleColourPalette component with jss

--- src/SingleColourPalette.js
+++ src/SingleColourPalette.js
@@ -5,6 +5,48 @@ import { generatePalette } from './colourHelpers';
 import ColourBox from './ColourBox';
 import NavBar from './NavBar';
 import PaletteFooter from './PaletteFooter';
+import styled from 'styled-components';
+
+const SingleColourPaletteContainer = styled('div')(() => ({
+  height: '100vh',
+  display: 'flex',
+  flexDirection: 'column',
+  '.Palette-colours': {
+    height: '90%',
+  },
+  '.go-back': {
+    width: '20%',
+    height: '50%',
+    margin: '0 auto',
+    display: 'inline-block',
+    position: 'relative',
+    cursor: 'pointer',
+    marginBottom: '-4px',
+    position: 'relative',
+    backgroundColor: 'black',
+    '& a': {
+      border: 'none',
+      outline: 'none',
+      fontSize: '1em',
+      display: 'inline-block',
+      width: '100px',
+      height: '30px',
+      position: 'absolute',
+      top: '50%',
+      left: '50%',
+      marginTop: '-15px',
+      marginLeft: '-50px',
+      textAlign: 'center',
+      lineHeight: '30px',
+      cursor: 'pointer',
+      backgroundColor: 'rgba(255, 255, 255, 0.3)',
+      transition: '0.4s ease',
+      textTransform: 'uppercase',
+      textDecoration: 'none',
+      color: 'white',
+    },
+  },
+}));
 
 export default function SingleColourPalette({ seedPalette }) {
   const [state, setState] = useState({ format: 'hex' });
@@ -44,17 +86,15 @@ export default function SingleColourPalette({ seedPalette }) {
   ));
 
   return (
-    <div className='SingleColourPalette Palette'>
+    <SingleColourPaletteContainer>
       <NavBar showSlider={false} changeFormat={changeFormat} />
       <div className='Palette-colours'>
         {colourBoxes}
-        <div className='ColourBox go-back'>
-          <Link to={`/palette/${paletteId}`} className='go-back-btn'>
-            Go Back
-          </Link>
+        <div className='go-back'>
+          <Link to={`/palette/${paletteId}`}>Go Back</Link>
         </div>
       </div>
       <PaletteFooter name={palette.paletteName} emoji={palette.emoji} />
-    </div>
+    </SingleColourPaletteContainer>
   );
 }
