bc1434128f40bcdb6eb9eb98ef8d7c7402c13a72
Oybek Abdulazizov <oybek.tulqinovich@gmail.com>
2023-04-25 18:38:47 +0200

    wire up colour picker with add colour button and add/display some example colours

--- src/NewPaletteForm.js
+++ src/NewPaletteForm.js
@@ -12,6 +12,7 @@ import LibraryAddIcon from '@mui/icons-material/LibraryAdd';
 import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
 import { ChromePicker } from 'react-color';
 import { Button } from '@mui/material';
+import chroma from 'chroma-js';
 
 const drawerWidth = 360;
 
@@ -57,31 +58,62 @@ const DrawerHeader = styled('div')(({ theme }) => ({
   padding: theme.spacing(0, 1),
   // necessary for content to be below app bar
   ...theme.mixins.toolbar,
-  justifyContent: 'flex-end',
+  justifyContent: 'flex-start',
 }));
 
 export default function NewPaletteForm() {
-  const [open, setOpen] = React.useState(false);
+  const [state, setState] = React.useState({
+    open: false,
+    currentColour: 'white',
+    colours: [],
+  });
 
   const handleDrawerOpen = () => {
-    setOpen(true);
+    setState((prevState) => ({
+      ...prevState,
+      open: true,
+    }));
   };
 
   const handleDrawerClose = () => {
-    setOpen(false);
+    setState((prevState) => ({
+      ...prevState,
+      open: false,
+    }));
+  };
+
+  const handleChangeComplete = (newColour) => {
+    setState((prevState) => ({
+      ...prevState,
+      currentColour: newColour.hex,
+    }));
+  };
+
+  const handleAddColour = () => {
+    const existingColour = state.colours.find(
+      (colour) => colour === state.currentColour
+    );
+    if (existingColour) {
+      alert('This colour already exists!');
+      return;
+    }
+    setState((prevState) => ({
+      ...prevState,
+      colours: [...state.colours, state.currentColour],
+    }));
   };
 
   return (
     <Box sx={{ display: 'flex' }}>
       <CssBaseline />
-      <AppBar position='fixed' open={open}>
+      <AppBar position='fixed' open={state.open}>
         <Toolbar>
           <IconButton
             color='inherit'
             aria-label='open drawer'
             onClick={handleDrawerOpen}
             edge='start'
-            sx={{ mr: 2, ...(open && { display: 'none' }) }}
+            sx={{ mr: 2, ...(state.open && { display: 'none' }) }}
           >
             <LibraryAddIcon />
           </IconButton>
@@ -101,10 +133,10 @@ export default function NewPaletteForm() {
         }}
         variant='persistent'
         anchor='left'
-        open={open}
+        open={state.open}
       >
         <DrawerHeader>
-          <IconButton onClick={handleDrawerClose}>
+          <IconButton onClick={handleDrawerClose} sx={{ ml: 1 }}>
             <ChevronLeftIcon />
           </IconButton>
         </DrawerHeader>
@@ -119,15 +151,32 @@ export default function NewPaletteForm() {
           </Button>
         </div>
         <ChromePicker
-          color='teal'
-          onChangeComplete={(newColour) => console.log(newColour.hex)}
+          color={state.currentColour}
+          onChangeComplete={handleChangeComplete}
         />
-        <Button variant='contained' color='primary'>
+        <Button
+          variant='contained'
+          onClick={handleAddColour}
+          style={{
+            backgroundColor: state.currentColour,
+            color:
+              chroma(state.currentColour).luminance() <= 0.4
+                ? 'white'
+                : 'black',
+          }}
+        >
           Add Colour
         </Button>
       </Drawer>
-      <Main open={open}>
+      <Main open={state.open}>
         <DrawerHeader />
+        <ul>
+          {state.colours.map((colour, id) => (
+            <li key={id} style={{ backgroundColor: colour }}>
+              {colour}
+            </li>
+          ))}
+        </ul>
       </Main>
     </Box>
   );
