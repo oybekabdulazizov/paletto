2850cfbfb1ba55d3d73976ae650657ca5cccda9b
Oybek Abdulazizov <oybek.tulqinovich@gmail.com>
2023-04-29 02:27:06 +0200

    beautify ColourPickerForm

--- src/ColourPickerForm.js
+++ src/ColourPickerForm.js
@@ -1,8 +1,27 @@
 import React, { useState, useEffect } from 'react';
 import { Button } from '@mui/material';
+import styled from 'styled-components';
 import { ValidatorForm, TextValidator } from 'react-material-ui-form-validator';
 import { ChromePicker } from 'react-color';
 import chroma from 'chroma-js';
+
+const ColourPickerWithStyles = styled('div')(() => ({
+  width: '100%',
+  '.colour-picker': {
+    width: '100% !important',
+    margin: '1rem 0',
+  },
+  '.colour-input': {
+    width: '100%',
+    margin: '6px auto',
+  },
+  '.add-colour-btn': {
+    width: '100%',
+    padding: '1em',
+    margin: '6px auto',
+    fontSize: '1.6em',
+  },
+}));
 
 export default function ColourPickerForm({
   addColour,
@@ -36,13 +55,18 @@ export default function ColourPickerForm({
   const handleChangeComplete = (newCol) => setNewColour(newCol.hex);
 
   return (
-    <>
-      <ChromePicker color={newColour} onChangeComplete={handleChangeComplete} />
+    <ColourPickerWithStyles>
+      <ChromePicker
+        className='colour-picker'
+        color={newColour}
+        onChangeComplete={handleChangeComplete}
+      />
       <ValidatorForm instantValidate={false} onSubmit={handleAddColour}>
         <TextValidator
           label='Colour Name'
           name='newColourName'
           value={newColourName}
+          className='colour-input'
           onChange={handleNewColourNameChange}
           validators={['required', 'isColourNameUnique', 'isColourUnique']}
           errorMessages={[
@@ -55,6 +79,7 @@ export default function ColourPickerForm({
           variant='contained'
           type='submit'
           disabled={paletteFull}
+          className='add-colour-btn'
           style={{
             backgroundColor: paletteFull ? 'gray' : newColour,
             color: chroma(newColour).luminance() <= 0.3 ? 'white' : 'black',
@@ -63,6 +88,6 @@ export default function ColourPickerForm({
           {paletteFull ? 'Palette Full' : 'Add Colour'}
         </Button>
       </ValidatorForm>
-    </>
+    </ColourPickerWithStyles>
   );
 }
